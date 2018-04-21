00ce3a87ef1add74075315ed68a6c34185df6b0c
Voikgrib <32133129+Voikgrib@users.noreply.github.com>
2018-04-21 18:16:18 +0300

    Add files via upload

---
+++ Graphics/libs/manager_lib.h
@@ -0,0 +1,96 @@
+
+//
+// This class realise parent game class 
+//
+class game_obj
+{
+	public:
+
+	sf::Sprite sprite;
+	
+	class my_vector<int> speed;
+	class my_vector<int> size;
+
+	bool is_alive;
+	
+	virtual void move() = 0;
+	virtual void draw() = 0;
+
+};
+
+class manager
+{
+	public:
+
+	class game_obj** massive;
+	int absolute_max;
+	int maximum;
+
+	  //==========================//
+	 // Constructor & destructor //
+	//==========================//
+
+	manager()
+	{
+		printf("manager created! \n");
+		maximum = 0;
+		massive = new game_obj* [Max_obj];
+		absolute_max = Max_obj;
+	}
+
+	~manager()
+	{
+
+		delete [] massive;
+
+	}
+
+	  //===========//
+	 // Functions //
+	//===========//
+
+	void add(class game_obj* new_elem)
+	{
+		if(absolute_max <= maximum)
+		{
+			printf("TOO MUCH OBJECTS!\n");
+			assert(false);
+		}
+		else
+			massive[maximum++] = new_elem;
+	}
+
+	void run()
+	{
+		int cur_elem = 0;
+
+		while(cur_elem != maximum)	
+		{
+			printf("all_move	1\n");
+			if(massive[cur_elem] != NULL) // ERROR HERE
+			{
+				printf("all_move	1.1\n");
+				printf("cur_elem = %p\n", massive[cur_elem]);
+				massive[cur_elem]->move();
+				massive[cur_elem]->draw();
+				printf("all_move	1.2\n");			
+			}
+
+			printf("all_move	2\n");
+
+			cur_elem++;
+		}
+
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
